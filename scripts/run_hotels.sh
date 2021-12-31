61bd9f70528103ab2de107e6c7bcc02ea5a15c66
Aarash Feizi <aarash95@gmail.com>
2021-12-31 18:09:46 -0500

    set gpu at the beginning

--- scripts/run_hotels.sh
+++ scripts/run_hotels.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+read -p "gpu: " GPU
+
 OUT_DIR="output"
 if [[ ! -d "${OUT_DIR}" ]]; then
     echo "Creating output dir for training : ${OUT_DIR}"
     mkdir ${OUT_DIR}
 fi
-CUDA_VISIBLE_DEVICES=0 python3 tools/main.py --cfg configs/hotels_resnet50.yaml
+CUDA_VISIBLE_DEVICES=$GPU python3 tools/main.py --cfg configs/hotels_resnet50.yaml
