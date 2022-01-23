2d439c38094170d7338860429ac9eac3094d74c1
Aarash Feizi <aarash95@gmail.com>
2022-01-22 19:14:55 -0500

    add auroc

--- ret_benchmark/engine/trainer.py
+++ ret_benchmark/engine/trainer.py
@@ -4,7 +4,7 @@
 # Contact: github@malong.com
 #
 # This source code is licensed under the LICENSE file in the root directory of this source tree.
-
+import copy
 import datetime
 import time
 
@@ -13,10 +13,68 @@ import torch
 import h5py
 import os
 
+from sklearn.metrics.pairwise import cosine_similarity
+
 from ret_benchmark.data.evaluations import RetMetric
 from ret_benchmark.utils.feat_extractor import feat_extractor
 from ret_benchmark.utils.freeze_bn import set_bn_eval
 from ret_benchmark.utils.metric_logger import MetricLogger
+
+
+def make_batch_bce_labels(labels):
+    """
+    :param labels: e.g. tensor of size (N,1)
+    :return: binary matrix of labels of size (N, N)
+    """
+
+    l_ = labels.repeat(len(labels)).reshape(-1, len(labels))
+    l__ = labels.repeat_interleave(len(labels)).reshape(-1, len(labels))
+
+    final_bce_labels = (l_ == l__).type(torch.float32)
+
+    # final_bce_labels.fill_diagonal_(0)
+
+    return final_bce_labels
+def get_samples(l, k):
+    if len(l) < k:
+        to_ret = np.random.choice(l, k, replace=True)
+    else:
+        to_ret = np.random.choice(l, k, replace=False)
+
+    return to_ret
+def get_xs_ys(bce_labels, k=1):
+    """
+
+    :param bce_labels: tensor of (N, N) with 0s and 1s
+    :param k: number of pos and neg samples per anch
+    :return:
+
+    """
+    xs = []
+    ys = []
+    bce_labels_copy = copy.deepcopy(bce_labels)
+    bce_labels_copy.fill_diagonal_(-1)
+    for i, row in enumerate(bce_labels_copy):
+        neg_idx = torch.where(row == 0)[0]
+        pos_idx = torch.where(row == 1)[0]
+
+        ys.extend(get_samples(neg_idx, k))
+        ys.extend(get_samples(pos_idx, k))
+        xs.extend(get_samples([i], 2 * k))
+
+    return xs, ys
+
+def calc_auroc(embeddings, labels):
+    from sklearn.metrics import roc_auc_score
+    bce_labels = make_batch_bce_labels(labels)
+    similarities = cosine_similarity(embeddings)
+
+    xs, ys = get_xs_ys(bce_labels)
+
+    true_labels = bce_labels[xs, ys]
+    predicted_labels = similarities[xs, ys]
+
+    return roc_auc_score(true_labels, predicted_labels)
 
 
 def __save_h5(data_description, data, data_type, path):
@@ -46,6 +104,7 @@ def do_train(
     start_iter = arguments["iteration"]
     best_iteration = -1
     best_recall = 0
+    best_auc = 0
 
     start_training_time = time.time()
     end = time.time()
@@ -59,16 +118,23 @@ def do_train(
             feats = feat_extractor(model, val_loader, logger=logger)
 
             ret_metric = RetMetric(feats=feats, labels=labels)
+
             recall_curr = ret_metric.recall_k(1)
+            auc_curr = calc_auroc(embeddings=feats, labels=torch.tensor(labels))
 
             if recall_curr > best_recall:
                 best_recall = recall_curr
+                best_auc = auc_curr
                 best_iteration = iteration
                 logger.info(f'Best iteration {iteration}: recall@1: {best_recall:.3f}')
+                logger.info(f'Best iteration {iteration}: auroc: {auc_curr:.3f}')
+
                 checkpointer.save(f"best_model")
                 checkpointer.tag_last_checkpoint("best_model.pth")
             else:
                 logger.info(f'Recall@1 at iteration {iteration:06d}: {recall_curr:.3f}')
+                logger.info(f'Auroc at iteration {iteration:06d}: {auc_curr:.3f}')
+
 
         model.train()
         model.apply(set_bn_eval)
@@ -125,7 +191,7 @@ def do_train(
         )
     )
 
-    logger.info(f"Best iteration: {best_iteration :06d} | best recall {best_recall} ")
+    logger.info(f"Best iteration: {best_iteration :06d} | best recall {best_recall} | best auroc {best_auc}")
 
 def save_embs(
         cfg,
